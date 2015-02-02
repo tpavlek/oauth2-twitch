2eb960f22eec24b9dc8d8b013be7d81ef46ef78b
Troy Pavlek <ebonwumon@depotwarehouse.net>
2015-02-02 14:43:57 -0700

    Added toArray to TwitchUser

--- src/Entity/TwitchUser.php
+++ src/Entity/TwitchUser.php
@@ -18,13 +18,24 @@ class TwitchUser
     /** @var  bool */
     protected $partnered;
 
-    public function __construct(array $attributes = array()) {
+    public function __construct(array $attributes = array())
+    {
         $this->id = $attributes['_id'];
         $this->display_name = $attributes['display_name'];
         $this->type = $attributes['type'];
         $this->bio = $attributes['bio'];
         $this->email = $attributes['email'];
         $this->partnered = $attributes['partnered'];
+    }
+
+    /**
+     * Get the contents of the user as a key-value array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return get_object_vars($this);
     }
 
     /**
